7ff7642780b04082087666ab426435da8ed5f3bb
rhrlfdud <87295663+rhrlfdud@users.noreply.github.com>
2024-06-20 00:44:17 +0900

    Update HelloController.java

--- src/main/java/com/mkyong/HelloController.java
+++ src/main/java/com/mkyong/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping(value={"/", "/hmpg")
+    @RequestMapping(value={"/", "/hmpg"})
     String hello() {
         return "Hello World, Spring Boot!";
     }
