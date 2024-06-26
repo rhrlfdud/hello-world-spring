c88bdce9a9305b39f9e74e8262e6308eb13d29bc
rhrlfdud <87295663+rhrlfdud@users.noreply.github.com>
2024-06-26 09:14:00 +0900

    Update HelloController.java

--- src/main/java/com/mkyong/HelloController.java
+++ src/main/java/com/mkyong/HelloController.java
@@ -1,6 +1,7 @@
 package com.mkyong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
