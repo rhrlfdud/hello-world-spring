5b22dcd94c3955fca068fa3a83ebb9956f05eb27
rhrlfdud <87295663+rhrlfdud@users.noreply.github.com>
2024-06-27 12:07:15 +0900

    Update TestController.java

--- src/main/java/com/mkyong/TestController.java
+++ src/main/java/com/mkyong/TestController.java
@@ -1,12 +1,13 @@
 package com.mkyong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping(value={"/version", "/admin/version"})
+    @RequestMapping(value={"/version", "/admin/version"}, method = RequestMethod.GET)
     String version() {
         return "Version : v1";
     }
