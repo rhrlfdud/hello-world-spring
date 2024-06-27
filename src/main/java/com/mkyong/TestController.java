c9010a3a4e9f6f3825ffb23d5c94c01e13c6304f
rhrlfdud <87295663+rhrlfdud@users.noreply.github.com>
2024-06-27 12:12:27 +0900

    Update TestController.java

--- src/main/java/com/mkyong/TestController.java
+++ src/main/java/com/mkyong/TestController.java
@@ -1,7 +1,5 @@
 package com.mkyong;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
